f87a4be2ad553bc5af01debc4fbd7afc030a0c6c
weihong.xu <xuweihong.cn@gmail.com>
2021-01-20 22:26:10 +0800

    chore: switch to offline version

--- update-noteself.sh
+++ update-noteself.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-#git subtree pull --prefix=noteself git@github.com:NoteSelf/NoteSelf.github.io.git master --squash
+git subtree pull --prefix=noteself git@github.com:NoteSelf/NoteSelf.github.io.git master --squash
 cp -f noteself/offline.html noteself/index.html 
 sed -i 's/noteself\.github\.io/link89\.github\.io/g' noteself/index.html
 
