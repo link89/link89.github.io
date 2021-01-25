7b0603d1565a2e55c870d3031fb80e3e08582bed
weihong.xu <xuweihong.cn@gmail.com>
2021-01-25 21:56:01 +0800

    fix(noteself): sw.js

--- update-noteself.sh
+++ update-noteself.sh
@@ -3,5 +3,5 @@ set -e
 cd "$(dirname "$0")"
 git subtree pull --prefix=noteself git@github.com:NoteSelf/NoteSelf.github.io.git master --squash
 cp -f noteself/offline.html noteself/index.html 
-sed -i 's/noteself\.github\.io/link89\.github\.io/g' noteself/index.html
-
+sed -i 's/noteself\.github\.io/link89\.github\.io\/noteself/g' noteself/index.html
+find . -name '*.js' -or -name '*.html' | xargs sed -i 's/\/sw.js/\/noteself\/sw.js/g'
